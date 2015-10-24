e99291d7184b2c01f7a436855c578de9a7bddc1b
Hackathon User <Hackathon User>
2015-10-25 07:53:37 +1100

    push live

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
